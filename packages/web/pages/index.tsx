a4b6c2a477b02d29a359139fc9fcf6e28fa255b0
Valtýr Örn Kjartansson <valtyr@gmail.com>
2021-09-26 23:39:42 +0000

    Stuff

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,15 +1,16 @@
-import type { NextPage } from "next";
-
+import Head from "next/head";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import Label from "../components/Label";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button buttonStyle="accent">Start experience</Button>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="max-w-lg">
+        <Label htmlFor="username" label="Username" />
+        <Input id="username" placeholder="Username" />
+        <Button buttonStyle="primary">Start experience</Button>
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
