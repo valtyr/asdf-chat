509cd069be042d660f401273b38c7f9c56280a37
Valtýr Örn Kjartansson <valtyr@gmail.com>
2021-09-27 18:55:00 +0000

    HMM

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
+import React from "react";
+import "tailwindcss/tailwind.css";
+
+import { IdProvider } from "@radix-ui/react-id";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <IdProvider>
+      <Component {...pageProps} />
+    </IdProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,16 +1,9 @@
 import Head from "next/head";
-import Button from "../components/Button";
-import Input from "../components/Input";
-import Label from "../components/Label";
+import RequiresUsername from "../components/common/RequiresUsername";
+import Button from "../components/widgets/Button";
+import Input from "../components/widgets/Input";
+import Label from "../components/widgets/Label";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="max-w-lg">
-        <Label htmlFor="username" label="Username" />
-        <Input id="username" placeholder="Username" />
-        <Button buttonStyle="primary">Start experience</Button>
-      </div>
-    </div>
-  );
+  return <RequiresUsername>Hello</RequiresUsername>;
 }
